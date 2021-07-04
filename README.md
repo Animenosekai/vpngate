d6a8502a9e578d7ed9210a0d314243d73f908360
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2021-07-04 23:33:30 +0200

    [add] Using the serverless api

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# VPNGate_ServerSelection
- 
+# vpngate
+
+A list of the VPN Servers available on VPNGate, using 'vpngate-api'
+
+## Backend
+
+For the serverless backend, please look at [Animenosekai/vpngate-api](https://github.com/Animenosekai/vpngate-api)
--- scripts/main.js
+++ scripts/main.js
@@ -1,108 +1,100 @@
-window.onload = function(){
+window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
-    const country = urlParams.get("country")
+    const country = urlParams.get("country")
     if (country == null) {
         document.getElementById("selectedCountry").innerText = "Loading, please wait..."
     } else {
         document.getElementById("selectedCountry").innerText = "Selected country: " + country
     }
 
-    
 
-    //fetch("https://animenosekai.herokuapp.com/vpngate/api/servers")
-    fetch("https://animenosekai.herokuapp.com/vpngate/api/servers" + window.location.search)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) {
+    fetch("https://vpngate-api.vercel.app/api/servers" + window.location.search)
+        .then((resp) => resp.json()) // Transform the data into json
+        .then((response) => {
+            if (response.success) {
+                if (country == null) {
+                    document.getElementById("selectedCountry").classList.add("hidden")
+                }
 
-        if (country == null) {
-            document.getElementById("selectedCountry").classList.add("hidden")
-        }
-        
-        document.getElementById("title").classList.add("loaded")
-        document.getElementById("selectedCountry").classList.add("loaded")
-        document.getElementById("loader").classList.add("hiddenLoader")
+                document.getElementById("title").classList.add("loaded")
+                document.getElementById("selectedCountry").classList.add("loaded")
+                document.getElementById("loader").classList.add("hiddenLoader")
 
-        setTimeout(function(){
-            document.getElementById("title").classList.add("done")
-                    
-            setTimeout(function(){
-                document.getElementById("loader").style.display = "none";
-                document.getElementById("serversList").classList.add("loaded")
-            }, 300)
-        }, 1000)
-        
-        
-        for (element in data) {
-            if (element == 0){
-                continue
+                setTimeout(() => {
+                    document.getElementById("title").classList.add("done")
+                    setTimeout(() => {
+                        document.getElementById("loader").style.display = "none";
+                        document.getElementById("serversList").classList.add("loaded")
+                    }, 300)
+                }, 1000)
+
+                let data = response.data
+
+
+                for (element in data) {
+                    var newServerElement = document.createElement("server")
+
+                    var newCountryElement = document.createElement("country")
+                    var newServerNameElement = document.createElement("serverName")
+                    var newNoSessionsElement = document.createElement("numberOfSessions")
+                    var newOperatorElement = document.createElement("operator")
+                    var newPingElement = document.createElement("ping")
+                    var newIpElement = document.createElement("ip")
+                    var newLogTypeElement = document.createElement("logType")
+                    var newSpeedElement = document.createElement("speed")
+                    var newScoreElement = document.createElement("score")
+
+                    newServerNameElement.innerText = data[element]["name"]
+                    newNoSessionsElement.innerText = data[element]["numberOfSessions"] + " Sessions"
+                    newOperatorElement.innerText = "by " + data[element]["operator"]
+                    newPingElement.innerText = data[element]["ping"] + "ms"
+                    newIpElement.innerText = data[element]["ip"]
+                    newLogTypeElement.innerText = data[element]["logType"]
+                    newScoreElement.innerText = data[element]["score"]
+                    newCountryElement.innerText = data[element]["countryLong"]
+                    var serverSpeed = parseInt(data[element]["speed"]) / 10e6
+                    newSpeedElement.innerText = String(Math.round((serverSpeed + Number.EPSILON) * 100) / 100) + 'Mbps'
+
+                    var newServerInfoElement = document.createElement("serverInfo")
+                    newServerInfoElement.appendChild(newCountryElement)
+                    newServerInfoElement.appendChild(newServerNameElement)
+                    newServerInfoElement.appendChild(newOperatorElement)
+                    newServerElement.appendChild(newServerInfoElement)
+
+                    var newServerDetailsElement = document.createElement("serverDetails")
+
+                    var newServerSpeedElement = document.createElement("serverSpeed")
+                    var SeparationElement = document.createElement("separation")
+                    SeparationElement.innerText = " / "
+                    newServerSpeedElement.appendChild(newPingElement)
+                    newServerSpeedElement.appendChild(SeparationElement)
+                    newServerSpeedElement.appendChild(newSpeedElement)
+
+                    newServerDetailsElement.appendChild(newServerSpeedElement)
+
+                    var newDownloadButton = document.createElement("button")
+                    newDownloadButton.id = "downloadBtn"
+                    newDownloadButton.innerText = "Download"
+                    newDownloadButton.setAttribute("onclick", "downloadConfigFile('" + String(data[element]['ip']) + "')")
+                    newServerDetailsElement.appendChild(newDownloadButton)
+
+
+                    newServerDetailsElement.appendChild(newNoSessionsElement)
+                    newServerDetailsElement.appendChild(newIpElement)
+
+                    newServerElement.appendChild(newServerDetailsElement)
+
+                    document.getElementById("serversList").appendChild(newServerElement)
+                }
+            } else {
+                document.getElementById("selectedCountry").innerText = response.data.message
             }
-            var newServerElement = document.createElement("server")
-            
-            var newCountryElement = document.createElement("country")
-            var newServerNameElement = document.createElement("serverName")
-            var newNoSessionsElement = document.createElement("numberOfSessions")
-            var newOperatorElement = document.createElement("operator")
-            var newPingElement = document.createElement("ping")
-            var newIpElement = document.createElement("ip")
-            var newLogTypeElement = document.createElement("logType")
-            var newSpeedElement = document.createElement("speed")
-            var newScoreElement = document.createElement("score")
-            
-            newServerNameElement.innerText = data[element]["ServerName"]
-            newNoSessionsElement.innerText = data[element]["NumberOfVPNSessions"] + " Sessions"
-            newOperatorElement.innerText = "by " + data[element]["Operator"]
-            newPingElement.innerText = data[element]["Ping"] + "ms"
-            newIpElement.innerText = data[element]["IP"]
-            newLogTypeElement.innerText = data[element]["LogType"]
-            newScoreElement.innerText = data[element]["Score"]
-            newCountryElement.innerText = data[element]["CountryLong"]
-            var serverSpeed = parseInt(data[element]["Speed"]) / 10e6
-            newSpeedElement.innerText = String(Math.round((serverSpeed + Number.EPSILON) * 100) / 100) + 'Mbps'
-            
-            var newServerInfoElement = document.createElement("serverInfo")
-            /*
-            var newServerDownloadLink = document.createElement("downloadLink")
-            newServerDownloadLink.innerText = "OpenVPN Config File"
-            newServerInfoElement.appendChild(newServerDownloadLink)
-            */
-            newServerInfoElement.appendChild(newCountryElement)
-            newServerInfoElement.appendChild(newServerNameElement)
-            newServerInfoElement.appendChild(newOperatorElement)
-            newServerElement.appendChild(newServerInfoElement)
-
-            var newServerDetailsElement = document.createElement("serverDetails")
-
-            var newServerSpeedElement = document.createElement("serverSpeed")
-            var SeparationElement = document.createElement("separation")
-            SeparationElement.innerText = " / "
-            newServerSpeedElement.appendChild(newPingElement)
-            newServerSpeedElement.appendChild(SeparationElement)
-            newServerSpeedElement.appendChild(newSpeedElement)
-
-            newServerDetailsElement.appendChild(newServerSpeedElement)
-
-            var newDownloadButton = document.createElement("button")
-            newDownloadButton.id = "downloadBtn"
-            newDownloadButton.innerText = "Download"
-            newDownloadButton.setAttribute("onclick", "downloadConfigFile('" + String(data[element]['IP']) + "')")
-            newServerDetailsElement.appendChild(newDownloadButton)
-
-
-            newServerDetailsElement.appendChild(newNoSessionsElement)
-            newServerDetailsElement.appendChild(newIpElement)
-            //newServerDetailsElement.appendChild(newLogTypeElement)
-            //newServerDetailsElement.appendChild(newScoreElement)
-
-            newServerElement.appendChild(newServerDetailsElement)
-
-            document.getElementById("serversList").appendChild(newServerElement)
-        }
-    })
-    .catch(function(err){
-        document.getElementById("selectedCountry").innerText = "An error occured"
-    })
+        })
+        .catch((err) => {
+            document.getElementById("selectedCountry").innerText = "An error occured"
+        })
 }
 
-function downloadConfigFile(serverIP){
-    window.open("https://animenosekai.herokuapp.com/vpngate/api/downloadConfig?serverIP=" + String(serverIP))
+function downloadConfigFile(serverIP) {
+    window.open("https://vpngate-api.vercel.app/api/servers?download=true&server=" + String(serverIP), "_self")
 }
